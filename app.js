428fd86009861e3ac4fc97ac5864b8e678bc3991
umairsolangi <51870556+umairsolangi@users.noreply.github.com>
2020-06-15 16:43:17 +0500

    JavaScript Assignment
    JavaScript Assignment

---
+++ app.js
@@ -0,0 +1,316 @@
+// Chapter Alerts Tast No 1 //
+// alert("Hellow World")
+
+
+// Chapter Alerts Task No 2 //
+// alert("Error! Plz Enter a Valid Password")
+
+
+// Chapter Alerts Task No 3 //
+// alert("Welcome To JS Land... Happy Coding") 
+
+
+// Chapter Alerts Task No 4 //
+// alert("Welcome To JS Land")
+// alert("Happy Coding")
+
+
+// Chapter Alerts Task No 5 //
+// console.log("Hellow.. I can run JS through browser's console")
+
+
+
+// Chapter Veriables For Stirng  Task No 1//
+// var username="Umair_Solangi";
+
+
+
+// Chapter Veriables For String Task No 2 //
+// var username;
+
+
+
+// Chapter Veriables For String Task No 3 //
+// var myname = "Umair_Solangi";
+// alert(myname)
+
+
+//Chapter Veriables For String Task No 4 //
+// var student_name = "Umair_Solangi";
+// var student_age = "19 Year";
+// var student_education = "Gradguage In Computer Science";
+// alert(student_name);
+// alert(student_age);
+// alert(student_education);
+
+
+
+// Chapter Veriables For String Task No 5 //
+// var dinner = "P";
+// alert(dinner+"");
+// alert(dinner+"i");
+// alert(dinner+"iz");
+// alert(dinner+"izz");
+// alert(dinner+"izza");
+
+
+
+// Chapter Veriables For String Task No 6 //
+// var email = "solangitv@gmail.com";
+// alert("My email address is " + email);
+
+
+
+// Chapter Veriables For String Task No 7 //
+// var book = " A Smater Way To Learn JavaScript";
+// alert("I'm trying to learn from the book"  + book) ;
+
+
+
+// Chapter Veriables For String Task No 8 //
+// document.write("Yah.. I can write HTML Content through JavaScript");
+
+
+
+// Chapter Veriables For String Task No 9 //
+// alert("__________@#%$#___________");
+// document.write("__________@#%$#___________");
+
+
+
+
+// Chapter Veriables For Numbers Task No 1 //
+// var age = "19"
+// alert("I am" +" " +  age + " " + (" year old"));
+
+
+
+// Chapter Veriables For Numbers Task No 3 //
+// var birth_year = "2000";
+// document.write("My Birth Year is" + birth_year);
+
+
+
+// Chapter Veriables For Numbers Task No 4 //
+// var visiter_name = "Umair_Solangi";
+// var product_title = "Hp Laptop";
+// var quantity = " 1";
+// document.write( visiter_name + "orderd" + quantity + product_title + "From RealStickLaptop.com");
+
+
+
+
+// Chapter Veriable Name legal illegal Task No 3 //
+// document.write( " <h1> Rule For Naming JS Veriables</h1>"); 
+// document.write("A heading stating “Rules for naming JS variables”");
+// document.write("Variable names can only contain ______, ______, and ______.");
+// document.write("Variables must begin with a ______, ______ or For example $name, _name or name");
+// document.write("Variable names are case _________");
+// document.write("Variable names should not be JS _________");
+
+
+
+
+// Chapter Math Expression Task No 1 //
+// var a = 10 ;
+// var b = 30 ;
+// var c = a+b ;
+// document.write("Sum of " + a + " " + "and" + " " + b + "  " + c);
+
+
+
+// Chapter Math Expression Task No 2 //
+// var a = 40 ;
+// var b = 30 ;
+// var c = a-b ;
+// document.write("Sum of " + a + " " + "and" + " " + b + "  " + c);
+
+
+
+
+// Chapter Math Expression Task No 3 //
+// var num1 = 10 ;
+// document.write("Value Of After Declaration is" + num1);
+// document.write("Value After Increment" + 11);
+// document.write("Value After Additon" + 20);
+
+
+
+// Chapter Math Expression Task No 4 //
+// var ticket = 600;
+// var total = 6000;
+// document.write("The Cost Of 5 Movie Ticket Is" + total);
+
+
+
+// Chapter Math Expression Task No 5 //
+// for ( var i = 1 ; i <= 10;  i++){
+//  document.write("4" + "x" + i + "=" + 4*i + "<br>")
+// }
+
+
+
+// Chapter Math Expression Task No 7 //
+// var price_item_1 = 45;
+// var price_item_2 = 56;
+// var quantity_item_1 = 2;
+// var quantity_item_2 = 3;
+// var shipping_charges = 200;
+// var total_item_1 = price_item_1*2+100;
+// var total_tiem_2 = price_item_2*3+100;
+// document.write("Price Of Item One is" + " " + price_item_1+ "<br>" 
+//                         +"Price Of Item Two is" + " " + price_item_2  + "<br>"
+//                         + "Quantity  Of Item One is" + " " + quantity_item_1 + "<br>"
+//                         + "Quantity  Of Item Two is" + " " + quantity_item_2 + "<br>" 
+//                         + "Shipping Charges" + " " + shipping_charges + "<br>" + "<br>"
+//                        +  "Total Cost Of Your Order is" + (total_item_1+total_tiem_2) );
+
+
+
+
+// Chapter Math Expression Task No 8 //
+// var total_marks = 1000;
+// var obtained_marks = 800;
+// var  percentage = obtained_marks/total_marks*100;
+// document.write("Total Marks:" + total_marks + "<br>" +
+//                            "Obtained Marks:" + obtained_marks + "<br>" +
+//                            "Percentage" + percentage );
+
+
+
+
+
+// Chapter Math Expression Task No 9 //
+// var us_dollar = 10;
+// var saudi_real = 25;
+// var pkr = us_dollar*104.80+saudi_real*28;
+// document.write("Total Currency In PKR:" + pkr);
+
+
+
+// Chapter Math Expression Task No 10 //
+// var a = 5;
+// var b = 10;
+// var c = 2 ;
+// var ans = a * b / c;
+// document.write(ans);
+
+
+
+
+// Chapter Math Expression Task No 11 //
+// var current_year = 2020;
+// var birth_year = 2000;
+// var age = current_year-birth_year;
+// document.write(age);
+
+
+
+// Chapter User Inputs or Conditional Statment  Tast No 1//
+// var city = prompt("Enter You City Name")
+// if (city = "karachi") {
+//     alert("Welcome In Karachi City")
+// }
+    
+
+
+
+// Chapter User Inputs or Conditional Statment  Tast No 2//
+// var gender = prompt("Enter You Genter")
+// if (gender= "male") {
+//     alert("Good Morning Sir")
+//         }
+// if (gender="female") {
+//     alert("Good Morning Ma'me")
+// }
+
+
+
+// Chapter User Inputs or Conditional Statment  Tast No 4//
+// var feul = prompt("Enter Your Remain Feul")
+// if(feul <= 0.25){
+//     alert("Plz Refill Your Tank")
+// }
+// else(feul === 1)
+//     alert("You Have Enough Feul for Survive")
+
+
+
+
+
+// Chapter User Inputs or Conditional Statment  Tast No 5//
+// var a = 4;
+// if (++a === 5){
+// alert("given condition for variable a is true");
+// }
+
+// var b = 82;
+// if (b++ === 83){
+// alert("given condition for variable b is true");
+// }
+// var c = 12;
+// if (c++ === 13){
+// alert("condition 1 is true");
+// }
+// if (c === 13){
+// alert("condition 2 is true");
+// }
+// if (++c < 14){
+// alert("condition 3 is true");
+// }
+// if(c === 14){
+// alert("condition 4 is true");
+// }
+// var materialCost = 20000;
+// var laborCost = 2000;
+// var totalCost = materialCost + laborCost;
+// if (totalCost === laborCost + materialCost){
+// alert("The cost equals");
+// }
+// if (true){
+// alert("True");
+// }
+// if (false){
+// alert("False");
+// }
+// if("car" < "cat"){
+//     alert("car is smaller than cat");
+//     }
+    
+
+
+
+// Chapter User Inputs or Conditional Statment  Tast No 6//
+// var eng = prompt("Enter Your English Marks");
+// var urdu = prompt("Enter Your Urdu Marks");
+// var mind = prompt("Enter Your Mind Science Marks");
+// var obtained_marks = (+eng)+(+urdu)+(+mind);
+// var total_marks = 300;
+// var percentage = obtained_marks/total_marks*100;
+// document.write("<h2>Total Marks:</h2>" + total_marks + "<br>" +
+//                           "<h2>Obtained Marks:</h2>" + (+obtained_marks) + "<br>" +
+//                           "<h2>Percentage:</h2>" + percentage + "<br>" )                        
+
+
+
+
+
+
+// Chapter User Inputs or Conditional Statment  Tast No 11//
+// var value1 = prompt("Enter Your Firsr Value")
+// var value2 = prompt("Enter Your Second Number")
+// var sign = prompt("Enter sign")
+// if ( sign === "+"){
+//     alert((+value1)+(+value2))
+// }else if ( sign === "-"){
+//     alert(value1-value2)
+// }else if(sign === "*"){
+//     alert(value1*value2)
+// }else if(sign==="/"){
+//     alert(value1/value2)
+// }
+
+
+
+
+// Chapter If else & else if Task No 1 //
